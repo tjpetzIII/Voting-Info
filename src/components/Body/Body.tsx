29ea10b642f0b3a7204d32cfd26520cb0b5bba62
Thomas Petz <tompetz2000@gmail.com>
2025-05-14 18:29:35 -0400

    Beginning of voting info project

---
+++ src/components/Body/Body.tsx
@@ -0,0 +1,9 @@
+export const Body = () => {
+  return (
+    <>
+      <h1>Enter Your Address</h1>
+      <h1>Voting Info</h1>
+      <h1>Next Elections</h1>
+    </>
+  );
+};
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+  return (
+    <header className="bg-white shadow-md">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16">
+          <div className="flex-shrink-0 text-xl font-bold text-blue-600">
+            Get Your Voting Information
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default { Header };
